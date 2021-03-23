41dace9f34ebe18e7de39b3e1eb70c010d9a2ae7
Chris <chrisjenney@email.arizona.edu>
2021-03-23 10:07:01 -0700

    Initial commit data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,13 @@
+# Simulate and plot data
+# Christopher Jenney
+# chrisjenney@email.arizona.edu
+# 2021-03-23
+
+# Simulate data from normal distribution
+x <- rnorm(n = 100)
+y <- 2* x + rnorm(n = 100, sd = 0.1)
+
+#plot simulated data
+plot(x = x, y = y)
+
+#save and share our work
